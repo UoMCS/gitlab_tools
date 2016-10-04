742625f411278d8a7414548ba5aa140563d52a6c
Chris <chris@starforge.co.uk>
2016-10-04 10:28:34 +0100

    Use same deep_clone as labfork for testing

--- deepfork.pl
+++ deepfork.pl
@@ -14,6 +14,42 @@ sub arg_error {
     die "Error: $message\nUsage: deepfork.pl <sourceID> <namespace> <projectname> [<user email>, ...]\n";
 }
 
+sub deep_clone {
+    my $api       = shift;
+    my $sourceid  = shift;
+    my $namespace = shift;
+    my $projname  = shift;
+    my $userids   = shift;
+
+    print "DEBUG: Doing deep fork of $sourceid... ";
+    # Do the actual fork into the admin user space
+    my $forkid = $api -> deep_fork($sourceid)
+        or die "Error: ".$api -> errstr()."\n";
+
+    print "Done.\nDEBUG: Doing rename of $forkid as $projname... ";
+    # Rename it so it can be moved sanely
+    $api -> rename_project($forkid, $projname)
+        or die $api -> errstr()."\n";
+
+    print "Done.\nDEBUG: Moving $forkid into namespace $namespace... ";
+    # And move it into the target namespace
+    $api -> move_project($forkid, $namespace)
+        or die $api -> errstr()."\n";
+    print "Done.\nFork: $namespace/$projname: $forkid\nDEBUG: Adding users...";
+
+    if(scalar(@{$userids})) {
+        my $userhash = {};
+        foreach my $userid (@{$userids}) {
+            $userhash -> {$userid} = $api -> {"api"} -> {"access_levels"} -> {"developer"};
+        }
+
+        $api -> set_users($forkid, $userhash)
+            or die $api -> errstr()."\n";
+    }
+    print "Done.\n"
+}
+
+
 my $config = Webperl::ConfigMicro -> new("config/gitlab.cfg")
     or die "Error: Unable to load configuration: $!\n";
 
@@ -28,36 +64,5 @@ my $api = GitLab::API::Utils -> new(url   => $config -> {"gitlab"} -> {"url"},
 # Convert the email address of users to internal IDs. This is a vital step
 # where failures should kill the script before anything is done.
 my $userids  = $api -> lookup_users(\@users);
-my $failures = 0;
-for(my $i = 0; $i < scalar(@users); ++$i) {
-    if(!$userids -> [$i]) {
-        warn "Unable to locate userid for user with address '".$users[$i]."'\n";
-        ++$failures;
-    }
-}
-die "One or more user lookups failed. Aborting process.\n"
-    if($failures);
 
-# Do the actual fork into the admin user space
-my $forkid = $api -> deep_fork($sourceid)
-    or die "Error: ".$api -> errstr()."\n";
-
-# Rename it so it can be moved sanely
-$api -> rename_project($forkid, $projname)
-    or die $api -> errstr()."\n";
-
-# And move it into the target namespace
-$api -> move_project($forkid, $namespace)
-    or die $api -> errstr()."\n";
-
-print "namespace/$projname: $forkid\n";
-
-if(scalar(@users)) {
-    my $userhash = {};
-    foreach my $userid (@{$userids}) {
-        $userhash -> {$userid} = $api -> {"api"} -> {"access_levels"} -> {"developer"};
-    }
-
-    $api -> set_users($forkid, $userhash)
-        or die $api -> errstr()."\n";
-}
+deep_clone($api, $sourceid, $namespage, $projname, $userids);
