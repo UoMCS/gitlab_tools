498ecadaeee3431534ae016de27d1645f42df7d1
Chris <chris@starforge.co.uk>
2018-09-07 15:28:24 +0100

    Allow notifications from bulkaccess.sh

--- bulkaccess.sh
+++ bulkaccess.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 echo "Setting $1 access for groups in $2"
+echo "Notifying $3"
 cd /home/chris/gitlabwork/gitlab_tools
-./setbulkaccess.pl $1 $2
+./setbulkaccess.pl $1 $2 | mailx -s "Setting $1 access for groups in $2" $3
